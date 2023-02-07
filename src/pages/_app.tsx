396d7f592c24c2d779d98e4e99a17bcf62ff6fff
Shiroe <aggelosteck@gmail.com>
2023-02-07 20:23:49 +0200

    updated font default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from '@next/font/google'
+import { MuseoModerno } from '@next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const museo = MuseoModerno({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
+    <style jsx global>
+      {`
+        :root {
+          --font-museo: ${museo.style.fontFamily};
+        }
+      `}
+    </style>
     <Component {...pageProps} />
   </>
 }
