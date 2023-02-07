f0b29b337687853099b809fe54d31819380976fa
Shiroe <aggelosteck@gmail.com>
2023-02-07 20:41:20 +0200

    updated formatting with prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { MuseoModerno } from '@next/font/google'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MuseoModerno } from '@next/font/google';
 
-const museo = MuseoModerno({ subsets: ['latin'] })
+const museo = MuseoModerno({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <style jsx global>
-      {`
-        :root {
-          --font-museo: ${museo.style.fontFamily};
-        }
-      `}
-    </style>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-museo: ${museo.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
