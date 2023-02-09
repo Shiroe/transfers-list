4785de9d14b6cd7bd0550d47c012f395906706cb
Shiroe <aggelosteck@gmail.com>
2023-02-09 20:07:30 +0200

    added colors and icon svgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+// const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,6 +9,18 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-museo)']
+      },
+      colors: {
+        brand: {
+          blue: '#2D3B4E',
+          green: '#50D8A5',
+          'green-dim': '#42C594',
+          yellow: '#FFC149',
+          sky: '#56A7FA',
+          pink: '#F96381',
+          violet: '#736BFF',
+          gray: '#F4F5F6'
+        },
       }
     },
   },
