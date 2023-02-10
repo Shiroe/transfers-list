f64d9f1901a8dd450d3a4df3ce29d1ca1d784598
Shiroe <aggelosteck@gmail.com>
2023-02-10 15:16:31 +0200

    removing css warning in console for autosizing image element on one axis

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -9,7 +9,7 @@ const TopNav = () => {
     <nav className="flex h-[71px] items-center justify-end bg-white pt-[13px] pb-[14px] pr-[19px] pl-[101px] shadow">
       <div className="mr-[9px] flex h-[44px] w-[44px] items-center justify-center">
         <Image
-          className={`h-[17px] w-[17px] bg-transparent object-contain`}
+          className={`h-[17px] w-auto bg-transparent object-contain`}
           src={profileSvg}
           alt={'profile'}
         />
