c95f50815a661d0e23b60853b06ae4427cdc8249
Shiroe <aggelosteck@gmail.com>
2023-02-10 19:03:28 +0200

    wip: playing with transfers list table rendering and grouping

--- src/components/TransfersList/index.tsx
+++ src/components/TransfersList/index.tsx
@@ -4,6 +4,12 @@ import TransfersHeading from './TransfersHeading';
 import { ITransfer } from '@/models/Transfer';
 
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('UTC');
 
 type TransfersListProps = {
   transfers?: ITransfer[] | null;
@@ -11,14 +17,63 @@ type TransfersListProps = {
 
 const TransfersList = ({ transfers }: TransfersListProps) => {
   const renderListByDates = () => {
-    if (!transfers) return;
-    const list: ITransfer[] = [...transfers];
+    /**
+     * WIP: brainstorming strategy for segregating fetched data
+     * on various use cases e.g. no today or tomorrow transfers
+     * and grouping the rest by same date
+     */
 
-    list.sort((a, b) => dayjs(a.datetime).diff(b.datetime));
-    console.log('LIST: ', list);
-    // make sure its sorted
+    if (!transfers) return null;
+    /**
+     * We assume that data is sorted from the backend
+     * by datetime when they're fetched, we know the mock data
+     * currently is as well.
+     */
 
-    return null;
+    /**
+     * Simulating the `today` variable to be our today so it
+     * matches the begining date of the fetched mock data so
+     * we can have the matching UI for the demo.
+     */
+    const today = dayjs.tz('2023-06-01');
+    const tomorrow = today.add(1, 'days');
+
+    const filteredToday = transfers.filter((t: ITransfer) =>
+      today.isSame(dayjs(t.datetime), 'date')
+    );
+
+    const filteredTomorrow = transfers.filter((t: ITransfer) =>
+      tomorrow.isSame(dayjs(t.datetime), 'date')
+    );
+
+    const todayTransfers = (
+      <>
+        <h3 className="text-sm font-semibold text-brand-blue text-opacity-50">
+          Today
+        </h3>
+        {filteredToday.map((t) => (
+          <div key={t.id}>{t.traveler_first_name}</div>
+        ))}
+      </>
+    );
+
+    const tomorrowTransfers = (
+      <>
+        <h3 className="text-sm font-semibold text-brand-blue text-opacity-50">
+          Tomorrow
+        </h3>
+        {filteredTomorrow.map((t) => (
+          <div key={t.id}>{t.traveler_first_name}</div>
+        ))}
+      </>
+    );
+
+    return (
+      <>
+        {filteredToday.length && todayTransfers}
+        {filteredTomorrow.length && tomorrowTransfers}
+      </>
+    );
   };
 
   if (!transfers) {
@@ -40,9 +95,6 @@ const TransfersList = ({ transfers }: TransfersListProps) => {
         Transfers
       </h2>
       <TransfersHeading />
-      <h3 className="text-sm font-semibold text-brand-blue text-opacity-50">
-        Today
-      </h3>
       {renderListByDates()}
     </>
   );
