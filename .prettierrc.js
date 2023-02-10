0a07c8ed07aa637d8ebbc678fd7875b6ca0daa27
Shiroe <aggelosteck@gmail.com>
2023-02-10 15:10:50 +0200

    updated prettier config for tailwind autosorting plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
+  tailwindConfig: './tailwind.config.js',
   printWidth: 80,
   semi: true,
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+  pluginSearchDirs: false,
 };
