a9e2a37d42093babe5dc1588b4a0ec50a1350cd2
Shiroe <aggelosteck@gmail.com>
2023-02-10 15:13:44 +0200

    updated sideNav with better svg components, might need refactoring in the future

--- src/assets/Icons/LiveIcon.tsx
+++ src/assets/Icons/LiveIcon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface IIcon {
   color?: string;
@@ -6,22 +6,31 @@ interface IIcon {
 }
 
 const LiveIcon = ({ color = '#2D3B4E', size = 32 }: IIcon) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
   return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+    <div
+      className="mb-[15px] flex h-[44px] w-[44px] cursor-pointer items-center justify-center rounded-[8px] transition-colors duration-150 ease-in-out hover:bg-brand-green"
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
     >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M12 24C5.37258 24 0 18.6274 0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24ZM9.21913 16.3753L11.9194 13H10C9.16149 13 8.69532 12.0301 9.21913 11.3753L13.2191 6.37531C13.5641 5.94404 14.1934 5.87412 14.6247 6.21913C15.056 6.56414 15.1259 7.19343 14.7809 7.6247L12.0806 11H14C14.8385 11 15.3047 11.9699 14.7809 12.6247L10.7809 17.6247C10.4359 18.056 9.80657 18.1259 9.37531 17.7809C8.94404 17.4359 8.87412 16.8066 9.21913 16.3753ZM22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-        fill={color}
-        fillOpacity="0.5"
-      />
-    </svg>
+      <svg
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        className="transition-colors duration-150 ease-in-out"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12 24C5.37258 24 0 18.6274 0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24ZM9.21913 16.3753L11.9194 13H10C9.16149 13 8.69532 12.0301 9.21913 11.3753L13.2191 6.37531C13.5641 5.94404 14.1934 5.87412 14.6247 6.21913C15.056 6.56414 15.1259 7.19343 14.7809 7.6247L12.0806 11H14C14.8385 11 15.3047 11.9699 14.7809 12.6247L10.7809 17.6247C10.4359 18.056 9.80657 18.1259 9.37531 17.7809C8.94404 17.4359 8.87412 16.8066 9.21913 16.3753ZM22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+          fill={isHovered ? '#fff' : color}
+          fillOpacity={isHovered ? '1' : '0.5'}
+        />
+      </svg>
+    </div>
   );
 };
 
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 
 import logoSvg from 'public/logo.svg';
-// import liveSvg from 'public/live.svg';
-import scheduledSvg from 'public/scheduled.svg';
-import statisticsSvg from 'public/statistics.svg';
-import revenueSvg from 'public/revenue.svg';
-import settingsSvg from 'public/settings.svg';
 
 import LiveIcon from '@/assets/Icons/LiveIcon';
+import ScheduledIcon from '@/assets/Icons/ScheduledIcon';
+import StatisticsIcon from '@/assets/Icons/StatisticsIcon';
+import RevenueIcon from '@/assets/Icons/RevenueIcon';
+import SettingsIcon from '@/assets/Icons/SettingsIcon';
 
 const SideNav = () => {
   return (
     <aside
       className={`
           absolute top-0 bottom-0 left-0 flex w-[82px] flex-col 
-          justify-between rounded-r-[8px] bg-white p-[17px] shadow transition-[width] ease-in hover:w-[200px]
+          justify-between rounded-r-[8px] bg-white p-[17px] shadow
         `}
     >
       <div className="overflow-hidden">
@@ -26,38 +25,11 @@ const SideNav = () => {
             alt="logo image"
           />
         </div>
-        <div className="mb-[15px] flex h-[44px] w-[44px] items-center justify-center">
-          <LiveIcon size={32} />
-          {/* <span>Live Icon</span> */}
-        </div>
-        <div className="mb-[15px] flex h-[44px] w-[44px] items-center justify-center rounded-[8px] bg-brand-green">
-          <Image
-            src={scheduledSvg}
-            className="h-[25px] w-[25px] object-contain"
-            alt="logo image"
-          />
-        </div>
-        <div className="mb-[15px] flex h-[44px] w-[44px] items-center justify-center">
-          <Image
-            src={statisticsSvg}
-            className="h-[32px] w-[32px] bg-transparent object-contain"
-            alt="logo image"
-          />
-        </div>
-        <div className="mb-[15px] flex h-[44px] w-[44px] items-center justify-center">
-          <Image
-            src={revenueSvg}
-            className="h-[32px] w-[32px] bg-transparent object-contain"
-            alt="logo image"
-          />
-        </div>
-        <div className="mb-[15px] flex h-[44px] w-[44px] items-center justify-center">
-          <Image
-            src={settingsSvg}
-            className="h-[32px] w-[32px] bg-transparent object-contain"
-            alt="logo image"
-          />
-        </div>
+        <LiveIcon />
+        <ScheduledIcon size={26} />
+        <StatisticsIcon />
+        <RevenueIcon />
+        <SettingsIcon />
       </div>
     </aside>
   );
