ecd69b093d537547b651b1f6d2dd47ac8d5af43b
Shiroe <aggelosteck@gmail.com>
2023-02-12 21:47:06 +0200

    added row heading component for date grouping

---
+++ src/components/TransfersList/TransfersRowHeading.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+
+interface RowHeading {
+  children: ReactNode;
+  className?: string;
+}
+
+const TransferRowHeading = ({ children, className, ...props }: RowHeading) => {
+  return (
+    <h3 {...props} className={`text-brand-blue text-opacity-50 ${className}`}>
+      {children}
+    </h3>
+  );
+};
+
+export default TransferRowHeading;
