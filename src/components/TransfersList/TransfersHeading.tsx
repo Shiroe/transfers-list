e9c9c0051aca533f9e596234f5dc5ecd562f39f3
Shiroe <aggelosteck@gmail.com>
2023-02-12 15:40:15 +0200

    updated styles of heading component to be proportionate to the container width

--- src/components/TransfersList/TransfersHeading.tsx
+++ src/components/TransfersList/TransfersHeading.tsx
@@ -5,23 +5,23 @@ const TransfersHeading = () => {
     <div
       className={`mb-[12px] flex h-[34px] items-center rounded-[8px] bg-brand-blue bg-opacity-[0.04] px-[15px] pb-[11px] pt-[10px]`}
     >
-      <span className="mr-[20px] w-[40px] text-xs font-semibold text-brand-blue text-opacity-50">
+      <span className="mr-[10px] w-[5%] text-xs font-semibold text-brand-blue text-opacity-50">
         STATUS
       </span>
-      <span className="mr-[100px] w-[65px] text-xs font-semibold text-brand-blue text-opacity-50">
+      <span className="mr-[10px] w-[16%] text-xs font-semibold text-brand-blue text-opacity-50">
         TRAVELER
       </span>
-      <span className="mr-[70px] w-[130px] text-xs font-semibold text-brand-blue text-opacity-50">
+      <span className="mr-[10px] w-[17.5%] text-xs font-semibold text-brand-blue text-opacity-50">
         ARRIVAL/DEPARTURE
       </span>
-      <span className="mr-[40px] w-[160px] text-xs font-semibold text-brand-blue text-opacity-50">
+      <span className="mr-[10px] w-[17.5%] text-xs font-semibold text-brand-blue text-opacity-50">
         FROM/TO
       </span>
-      <span className="mr-[5px] text-xs font-semibold text-brand-blue text-opacity-50">
+      <span className="flex w-[19%] items-center text-xs font-semibold text-brand-blue text-opacity-50">
         OPPORTUNITIES
-      </span>
-      <span className="flex h-[20px] items-center justify-center rounded-[2px] bg-brand-yellow px-[3px] text-xxs text-white shadow shadow-brand-yellow">
-        NEW
+        <span className="ml-[5px] flex h-[20px] items-center justify-center rounded-[2px] bg-brand-yellow px-[3px] text-xxs font-bold text-white shadow shadow-brand-yellow">
+          NEW
+        </span>
       </span>
     </div>
   );
