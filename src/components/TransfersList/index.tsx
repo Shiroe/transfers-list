cd0f8873d52f94cbde7c964fe9aa317b971be1b5
Shiroe <aggelosteck@gmail.com>
2023-02-13 00:35:21 +0200

    fixed and simplified grouping logic for different dates and when to put new header component

--- src/components/TransfersList/index.tsx
+++ src/components/TransfersList/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 
 import TransfersHeading from './TransfersHeading';
 import TransferRowHeading from './TransfersRowHeading';
@@ -21,7 +21,7 @@ type TransfersListProps = {
 
 const TransfersList = ({ transfers }: TransfersListProps) => {
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [transfersPerPage] = useState<number>(10);
+  const [transfersPerPage] = useState<number>(9);
 
   const sortedTransfers = [...transfers].sort(
     (a, b) => dayjs(a.datetime).valueOf() - dayjs(b.datetime).valueOf()
@@ -58,30 +58,33 @@ const TransfersList = ({ transfers }: TransfersListProps) => {
       {currentTransfers.reduce<ReactNode[]>((acc, transfer, index, arr) => {
         const date = dayjs(transfer.datetime);
         let day;
-        if (dayjs().isSame(date, 'day')) {
+
+        // simulating current date to today;
+        const today = dayjs('2023-06-01');
+        const tomorrow = today.add(1, 'day');
+
+        if (today.isSame(date, 'day')) {
           day = 'Today';
-        } else if (dayjs().add(1, 'day').isSame(date, 'day')) {
+        } else if (tomorrow.isSame(date, 'day')) {
           day = 'Tomorrow';
         } else {
-          day = date.format('MM/DD/YYYY');
+          day = date.format('dddd, D MMM');
         }
-        if (
-          index === 0 ||
-          day !==
-            (dayjs(arr[index - 1]?.datetime).isSame(date, 'day')
-              ? 'Today'
-              : dayjs(arr[index - 1]?.datetime)
-                  .add(1, 'day')
-                  .isSame(date, 'day')
-              ? 'Tomorrow'
-              : date.format('MM/DD/YYYY'))
-        ) {
+
+        const previousTransferDate =
+          index > 0 ? dayjs(arr[index - 1]?.datetime) : null;
+        const isSameAsPrev = previousTransferDate
+          ? previousTransferDate.isSame(date, 'day')
+          : false;
+
+        if (index === 0 || !isSameAsPrev) {
           acc.push(
             <TransferRowHeading className="mb-[12px]" key={day}>
               {day}
             </TransferRowHeading>
           );
         }
+
         acc.push(
           <TransferItem className="mb-[7px]" key={transfer.id} {...transfer} />
         );
