ba8c8d0abd7a8cc4169b258d397701808ce43041
Shiroe <aggelosteck@gmail.com>
2023-02-15 20:09:28 +0200

    updated transferItem component styles for mobile responsiveness

--- src/components/TransfersList/TransferItem.tsx
+++ src/components/TransfersList/TransferItem.tsx
@@ -24,6 +24,7 @@ const TransferItem = ({
   id,
   datetime,
   category,
+  property_title,
   traveler_photo,
   traveler_first_name,
   traveler_last_name,
@@ -41,9 +42,9 @@ const TransferItem = ({
     <div
       key={id}
       onClick={onClick}
-      className={`flex items-center rounded-[6px] bg-white py-[9px] px-[15px] text-regular text-brand-blue shadow-sm ${className}`}
+      className={`flex flex-col items-start rounded-[6px] bg-white pl-[27px] pt-[20px] pb-[22px] pr-[20px] text-regular text-brand-blue shadow-sm md:flex-row md:items-center md:py-[9px] md:px-[15px] ${className}`}
     >
-      <Status className="mr-[10px] w-[5%]">
+      <Status className="order-1 hidden md:mr-[10px] md:block md:w-[5%]">
         {category === 'Arrival' ? (
           <ArrivalIcon size={38} />
         ) : category === 'Departure' ? (
@@ -52,52 +53,90 @@ const TransferItem = ({
           <InCityIcon size={38} />
         )}
       </Status>
-      <Avatar className="mr-[10px] flex w-[16%] items-center justify-start">
-        <div className="mr-[13px] min-h-[38px] min-w-[38px]">
+      {/* Should find a better approach to serving those icons so we can have more freedom with css queries on responsiveness */}
+      <Status
+        className={`order-1 flex items-center rounded-[16px] py-[1px] pl-[4px] pr-[6px] md:hidden ${
+          category === 'Arrival'
+            ? 'bg-brand-sky'
+            : category === 'Departure'
+            ? 'bg-brand-pink'
+            : 'bg-brand-violet'
+        }`}
+      >
+        {category === 'Arrival' ? (
+          <ArrivalIcon size={16} />
+        ) : category === 'Departure' ? (
+          <DepartureIcon size={16} />
+        ) : (
+          <InCityIcon size={16} />
+        )}
+        <span className="ml-[2px] text-xxs text-white">{category}</span>
+      </Status>
+      <Avatar className="order-2 -mt-[30px] flex w-full flex-row-reverse items-baseline justify-between md:mr-[10px] md:mt-[unset] md:w-[16%] md:flex-row md:items-center md:justify-start">
+        <div className="min-h-[38px] min-w-[38px] md:mr-[13px]">
           <Image
             src={traveler_photo}
             width={38}
             height={38}
             alt="traveler photo"
-            className="h-[38px] w-[38px] rounded-full object-cover"
+            className="h-[55px] w-[55px] translate-y-[10px] rounded-full object-cover md:h-[38px] md:w-[38px] md:translate-y-[unset]"
           />
         </div>
-        <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+        <div className="overflow-hidden -text-ellipsis whitespace-nowrap text-mobile-lg font-semibold md:mt-[unset] md:text-regular md:font-normal">
           {traveler_first_name} {traveler_last_name}
-        </span>
+        </div>
       </Avatar>
-      <DateTime className="mr-[10px] w-[17.5%] ">
+      {property_title && (
+        <div className="order-3 mt-[20px] mb-[15px] md:hidden">
+          <p className="mb-[2px] text-xxs font-semibold tracking-[1px] text-brand-blue text-opacity-50">
+            PROPERTY
+          </p>
+          <p className="text-md text-brand-blue">{property_title}</p>
+        </div>
+      )}
+      <DateTime className="order-3 mb-[15px] md:mb-[unset] md:mr-[10px] md:w-[17.5%]">
+        <p className="mb-[2px] text-xxs font-semibold tracking-[1px] text-brand-blue text-opacity-50 md:hidden">
+          ARRIVAL TIME
+        </p>
         {dayjs(datetime).format('ddd, D MMM HH:mm')}
       </DateTime>
-      <LocationTitle className="mr-[10px] w-[17.5%] overflow-hidden text-ellipsis whitespace-nowrap">
+      <LocationTitle className="text-ellipsis order-4 mb-[15px] overflow-hidden whitespace-nowrap md:mb-[unset] md:mr-[10px] md:w-[17.5%]">
+        <p className="mb-[2px] text-xxs font-semibold tracking-[1px] text-brand-blue text-opacity-50 md:hidden">
+          FROM
+        </p>
         {location_title}
       </LocationTitle>
-      <TransferOpportunities className="flex w-[19%] items-center gap-3">
-        {babies && (
-          <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
-            <BabyIcon size={24} />
-          </div>
-        )}
-        {early_checkin && (
-          <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
-            <SunIcon size={24} />
-          </div>
-        )}
-        {late_checkout && (
-          <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
-            <MoonIcon size={24} />
-          </div>
-        )}
-        {return_transfer && (
-          <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
-            <ReturnTransferIcon size={24} />
-          </div>
-        )}
-        {!opportunityExists && (
-          <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full border-[3px] border-brand-gray">
-            <div className="h-[2px] w-[6px] bg-brand-gray-dark"></div>
-          </div>
-        )}
+      <TransferOpportunities className="order-5 md:w-[19%]">
+        <p className="mb-[5px] block text-xxs font-semibold tracking-[1px] text-brand-blue text-opacity-50 md:hidden">
+          OPPORTUNITIES
+        </p>
+        <div className="flex items-center gap-3">
+          {babies && (
+            <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
+              <BabyIcon size={24} />
+            </div>
+          )}
+          {early_checkin && (
+            <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
+              <SunIcon size={24} />
+            </div>
+          )}
+          {late_checkout && (
+            <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
+              <MoonIcon size={24} />
+            </div>
+          )}
+          {return_transfer && (
+            <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full bg-brand-gray">
+              <ReturnTransferIcon size={24} />
+            </div>
+          )}
+          {!opportunityExists && (
+            <div className="flex h-[38px] w-[38px] items-center justify-center rounded-full border-[3px] border-brand-gray">
+              <div className="h-[2px] w-[6px] bg-brand-gray-dark"></div>
+            </div>
+          )}
+        </div>
       </TransferOpportunities>
     </div>
   );
