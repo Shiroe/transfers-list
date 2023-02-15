12d152871fa326829697583d90547530e96216af
Shiroe <aggelosteck@gmail.com>
2023-02-15 20:08:49 +0200

    updated styles for row header separator on mobile screens

--- src/components/TransfersList/TransfersRowHeading.tsx
+++ src/components/TransfersList/TransfersRowHeading.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import CalendarIcon from '@/assets/Icons/CalendarIcon';
 
 interface RowHeading {
   children: ReactNode;
@@ -9,8 +10,15 @@ const TransferRowHeading = ({ children, className, ...props }: RowHeading) => {
   return (
     <h3
       {...props}
-      className={`my-[12px] text-brand-blue text-opacity-50 ${className}`}
+      className={`my-[12px] flex items-center rounded-[16px] bg-brand-blue
+        bg-opacity-[0.04] px-[12px] py-[9px] text-regular text-brand-blue 
+        text-opacity-50 md:rounded-none md:bg-transparent md:px-[unset] md:py-[unset]
+        md:text-sm ${className}
+      `}
     >
+      <div className="mr-[10px] md:hidden">
+        <CalendarIcon size={14} />
+      </div>
       {children}
     </h3>
   );
